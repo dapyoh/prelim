cdacb3a7e5958f60332cb636163e69c636df28a7
dapyoh <louietabiliran@gmail.com>
2021-09-20 16:44:13 +0800

    First Commit

---
+++ resources/views/pages/support.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Support Page</h1>
+    <a href="{{url('/')}}">Home</a> | <a href="{{url('/profile')}}">Profile</a> | Support
+    
+</body>
+</html>
+
+<?php
+
+use Illuminate\Support\Facades\Route;
